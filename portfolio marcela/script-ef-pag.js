6fadb9fa5a768cd0c237db9ad6e9399f7b3a5e9b
araolive <85970126+araolive@users.noreply.github.com>
2024-05-20 13:59:17 -0400

    Add files via upload

---
+++ portfolio marcela/script-ef-pag.js
@@ -0,0 +1,43 @@
+
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.container-about',{
+    rotate:{x: 0, y: 80, z: 0},
+    duration: 1000});
+
+sr.reveal('.foto-marcela-sobre',{
+    rotate:{x: 0, y: 80, z: 0},
+    duration: 1000});
+
+sr.reveal('.tonalidade-redes-sociais-t',{
+    rotate:{x: 0, y: 80, z: 0},
+    duration: 2000});
+    
+sr.reveal('.depoimentos-gerados-clientes',{
+    rotate:{x: 0, y: 80, z: 0},
+    duration: 1000});
+
+sr.reveal('.tema-timeline-etapas-consulta',{
+    rotate:{x: 0, y: 80, z: 0},
+    duration: 1000});
+
+sr.reveal('.quadrados-com-textos',{
+    rotate:{x: 0, y: 80, z: 0},
+    duration: 1000});
+
+sr.reveal('.tema',{
+    rotate:{x: 0, y: 80, z: 0},
+    duration: 1000});
+    
+
+
+sr.reveal('.primeiro-elemento', {duration: 1000});
+sr.reveal('.elemento1', {duration: 1000});
+sr.reveal('.call-to-action-especialidades', {duration: 1000});
+sr.reveal('.query-texto', {duration: 1000});
+sr.reveal('.tema-depoimentos', {duration: 1000});
+sr.reveal('.texto-c-p-o', {duration: 1000});
+sr.reveal('.texto-faq', {duration: 1000});
+sr.reveal('.elementos-do-sumary', {duration: 1000});
+sr.reveal('.container-informacoes-footer', {duration: 1000});
+sr.reveal('.imagem-c-p-o', {duration: 1000});
